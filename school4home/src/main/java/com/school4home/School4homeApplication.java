f6349161912d885191a1def1b66bb50f078ea068
cfnunes <cfnunes@stefanini.com>
2018-08-27 19:57:48 -0300

    Create spring boot project

---
+++ school4home/src/main/java/com/school4home/School4homeApplication.java
@@ -0,0 +1,12 @@
+package com.school4home;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class School4homeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(School4homeApplication.class, args);
+	}
+}
