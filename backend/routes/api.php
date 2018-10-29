05150171eabaf128f3c3fcca0827e33d75660892
Victor da Silva Fonseca Leite <victorsfleite@gmail.com>
2018-10-29 11:58:14 -0200

    Create Auth features
    - Login
    - Logout
    - Register
    - Logged user

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,18 +1,22 @@
 <?php
 
-use Illuminate\Http\Request;
-
 /*
 |--------------------------------------------------------------------------
 | API Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
+| Here is where you can register the API routes for your application as
+| the routes are automatically authenticated using the API guard and
+| loaded automatically by this application's RouteServiceProvider.
 |
-*/
+ */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['prefix' => 'auth'], function () {
+    Route::group(['middleware' => 'auth:api'], function () {
+        Route::get('user', 'Auth\AuthController@user');
+        Route::get('logout', 'Auth\AuthController@logout');
+    });
+
+    Route::post('login', 'Auth\AuthController@login');
+    Route::post('register', 'Auth\AuthController@register');
 });
