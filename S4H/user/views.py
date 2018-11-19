e29eda4fc75019d828122ace31c526be8ed0bf2f
Vitor Menezes <menezesvitorg@gmail.com>
2018-11-19 09:16:36 -0200

    Created login view

--- S4H/user/views.py
+++ S4H/user/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from .forms import PasswordForm
-from .forms import UserForm
+from .forms import UserForm, LoginForm
 from django.contrib.auth import login, logout
 from django.contrib.auth.models import User
 from .models import S4HUser
@@ -8,6 +8,7 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.views.generic.edit import FormView
 from django.views import View
+from S4H.views import index
 
 class NewUserView(FormView):
     template_name = "user/newUser.html"
@@ -18,3 +19,19 @@ class NewUserView(FormView):
         form.insert()
         messages.success(self.request, 'Voce foi cadastrado')
         return super(NewUserView, self).form_valid(form)
+
+class LoginView(FormView):
+    template_name = "user/newUser.html"
+    form_class = LoginForm
+    success_url = "/"
+
+    def form_valid(self, form):
+        user = form.authenticate_user()
+        login(self.request, user)
+        return super(LoginView, self).form_valid(form)
+
+    def form_invalid(self, form):
+        return index(self.request, login_form=form)
+
+    def get(self, request, *args, **kwargs):
+        return redirect('index')
