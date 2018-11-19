dfc772c2c4f697fafe3e3d017b0f66deb4e70fe8
Vitor Menezes <menezesvitorg@gmail.com>
2018-11-19 09:32:32 -0200

    Created logout and delete user urls

--- S4H/user/urls.py
+++ S4H/user/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url
-from .views import LoginView
+from .views import LoginView, logout_user, delete_user
 from .views import NewUserView
 
 urlpatterns = [
     url(r'newuser/', NewUserView.as_view(), name='newuser'),
     url(r'login/', LoginView.as_view(), name='login'),
+    url(r'logout/', logout_user, name='logout'),
+    url(r'delete/$', delete_user, name='deleteuser'),
 ]
