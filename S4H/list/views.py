ff7afe6dcace31b4d95ebb634a2bfd09b666d9d2
Thiago Pereira <thiagoflames@gmail.com>
2018-11-22 11:42:06 -0200

    build the create_question_text view and configs of the list app

--- S4H/list/views.py
+++ S4H/list/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.shortcuts import render
+from django.urls import reverse
+from django.http import HttpResponseRedirect
+from django.http import HttpResponse
+
+from .forms import QuestionTextForm
 
 # Create your views here.
+def create_question_text(request):
+    title = "Crie uma questão"
+    if request.method == "POST":
+        form = QuestionTextForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse("index"))
+        return render(request, 'question_text.html', {'form': form,'title':title})
+
+    else:
+        form = QuestionTextForm()
+        return render(request, 'question_text.html', {'form': form,'title':title})
